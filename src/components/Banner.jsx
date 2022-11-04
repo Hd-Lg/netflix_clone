90ffba8ddd10b96ede013927b581a89259291dd6
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2022-11-04 03:07:12 +0100

    Done API implementation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,6 @@ export default function Banner() {
 	useEffect(() => {
 		async function fetchData() {
 			const request = await axios.get(requests.fetchNetflixOriginals);
-			console.log(`'Text' + ${requests.fetchNetflixOriginals}`);
 
 			setMovie(
 				request.data.results[
@@ -21,7 +20,6 @@ export default function Banner() {
 		fetchData();
 	}, []);
 
-	console.log(movie);
 	function truncate(string, n) {
 		return string?.length > n ? string.substr(0, n - 1) + "..." : string;
 	}
@@ -30,20 +28,22 @@ export default function Banner() {
 			className="banner"
 			style={{
 				backgroundSize: "cover",
-				backgroundImage: `url("https://tecstaff.jp/wp-content/uploads/2018/09/netflix_banner.jpg")`,
+				backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie?.backdrop_path})`,
 				backgroundPosition: "center center",
 			}}>
 			<div className="banner__contents">
-				<h1 className="banner__title">Movie Name</h1>
+				<h1 className="banner__title">
+					{movie?.title || movie?.name || movie?.original_name}
+				</h1>
 				<div className="banner__buttons">
 					<button className="banner__button">Play</button>
 					<button className="banner__button">My List</button>
 				</div>
 				<h1 className="banner__description">
-					{truncate(`Description`, 150)}
+					{truncate(movie?.overview, 150)}
 				</h1>
 			</div>
-			<div className="banner--fadeBottom" />
+			{/* <div className="banner--fadeBottom" /> */}
 		</header>
 	);
 }
