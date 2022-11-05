9d51f3a1f92e09f99c5a470dde4e42a4e5176dd4
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2022-11-05 17:33:34 +0100

    Done Firestore register & auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
+import { auth } from "./firebase";
 import { HomeScreen, Login } from "./screens";
 import "./App.css";
 
 export default function App() {
 	const user = null;
+
+	useEffect(() => {
+		// Like a listener, store login in cookie
+		const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+			if (userAuth) {
+				// logged in
+				console.log(userAuth);
+			} else {
+				// logged out
+			}
+		});
+		return unsubscribe;
+	}, []);
 
 	return (
 		<div className="App">
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 const AUTH_DOMAIN = process.env.REACT_APP_AUTH_DOMAIN;
@@ -21,4 +20,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
