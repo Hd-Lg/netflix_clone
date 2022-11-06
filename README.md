77c9a9c12a38cc44695169caff6cf7f1cb8b681b
Hd-Lg <59037834+Hd-Lg@users.noreply.github.com>
2022-11-07 00:35:32 +0100

    Added details subscription UI

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Stack:
 - Stripe with TEST API,
 - Firebase, Firebase Authentication & Cloud Firestore,
 - Redux Store
+
+You can create an account by entering a fake email address.
+Test the Stripe integration by entering fake data in it: 4242 4242 4242 4242, etc.
--- src/screens/PlansScreen.jsx
+++ src/screens/PlansScreen.jsx
@@ -10,6 +10,25 @@ import "./PlansScreen.css";
 function PlansScreen() {
 	const [products, setProducts] = useState("");
 	const user = useSelector(selectUser);
+	const [subscription, setSubscription] = useState(null);
+
+	useEffect(() => {
+		db.collection("customers")
+			.doc(user.uid)
+			.collection("subscriptions")
+			.get()
+			.then((querySnapshot) => {
+				querySnapshot.forEach(async (subscription) => {
+					setSubscription({
+						role: subscription.data().role,
+						current_period_end:
+							subscription.data().current_period_end.seconds,
+						current_period_start:
+							subscription.data().current_period_start.seconds,
+					});
+				});
+			});
+	}, [user.uid]);
 
 	useEffect(() => {
 		db.collection("products")
@@ -63,18 +82,34 @@ function PlansScreen() {
 
 	return (
 		<div className="plansScreen">
+			{subscription && (
+				<p>
+					Renewal date:{" "}
+					{new Date(
+						subscription?.current_period_end * 1000
+					).toLocaleDateString()}
+				</p>
+			)}
 			{Object.entries(products).map(([productId, productData]) => {
+				const isCurrentPackage = productData.name
+					?.toLowerCase()
+					.includes(subscription?.role);
 				return (
-					<div key={productId} className="plansScreen__plan">
+					<div
+						key={productId}
+						className={`${
+							isCurrentPackage && "plansScreen__plan--disable"
+						} plansScreen__plan`}>
 						<div className="plansScreen__info">
 							<h5>{productData.name}</h5>
 							<h6>{productData.description}</h6>
 						</div>
 						<button
 							onClick={() =>
+								!isCurrentPackage &&
 								loadCheckOut(productData.prices.priceId)
 							}>
-							Subscribe
+							{isCurrentPackage ? "Current package" : "Subscribe"}
 						</button>
 					</div>
 				);
